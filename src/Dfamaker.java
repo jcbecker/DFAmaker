ec6b32b3f4efddde9295ae2a08d2969c871da1be
jcbecker <joaocarlosbecker666@gmail.com>
2016-05-18 16:10:55 -0300

    separando cadeias das gramáticas

--- src/Dfamaker.java
+++ src/Dfamaker.java
@@ -4,7 +4,8 @@ import java.util.*;
 class Dfamaker{
     public static void main (String args[]){
         List<String> rawinput = new ArrayList<String>();
-        Grammar test = new Grammar();
+        List<Integer> spacer= new ArrayList<Integer>();
+        List<Grammar> grammars = new ArrayList<Grammar>();
         try{
             File f= new File(args[0]);
     		Scanner s= new Scanner(f);
@@ -14,11 +15,27 @@ class Dfamaker{
             System.out.println(rawinput.size());
             for(int i=0;i<rawinput.size();i++){
                 if(rawinput.get(i).trim().isEmpty()){
-                    System.out.println("Line "+(i+1)+" is empty");
+                    //System.out.println("Line "+(i+1)+" is empty");
+                    spacer.add(i);  
                 }else{
                     System.out.println(rawinput.get(i));
                 }
             }
+            for(int i=0;i<spacer.size();i++){
+                System.out.println(spacer.get(i));
+            }
+            String[] strinput = new String [spacer.get(0)]; 
+            strinput=rawinput.subList(0,spacer.get(0)).toArray(strinput);
+            System.out.println(strinput.length);
+            for (int i = 0;i<strinput.length; i++){
+                System.out.println(strinput[i]);
+            }
+            
+            
+            
+            
+            
+            
             
         }catch(IOException e){
             System.out.println(e);
